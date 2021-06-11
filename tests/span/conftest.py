b9ccaab5513cdb9c1dcc94d42e649652cb937593
vcheketx <viktorx.cheketa@intel.com>
2021-06-11 05:31:09 -0700

    debug

--- tests/span/conftest.py
+++ tests/span/conftest.py
@@ -3,6 +3,7 @@ Conftest file for span tests
 '''
 
 import pytest
+#from span_helpers import 
 
 @pytest.fixture(scope="module")
 def cfg_facts(duthosts, rand_one_dut_hostname):
@@ -35,26 +36,21 @@ def ports_for_test(cfg_facts):
 
     # Select 3 ports for test
     ports = cfg_facts['VLAN_MEMBER']['Vlan{}'.format(vlan)]
-    port_names = ports.keys()
-    selected_ports = [port_names[0], port_names[1], port_names[-1]]
+    lags = cfg_facts['PORTCHANNEL'].keys()
+    monitor = ports.pop[0]
 
-    # Generate port info for selected ports
-    port_info = []
-    for port in selected_ports:
-        port_info.append({'name': port,
-                          'tagging_mode': ports[port]['tagging_mode'],
-                          'index': cfg_facts['port_index_map'][port]}
-                        )
 
+    # Return dict with names of ports, available for mirror session, 
+    # list of other ports and LAGs, and monitor port
     return {
-        'source1': port_info[0],
-        'source2': port_info[1],
-        'monitor': port_info[2],
+        'lags': lags,
+        'ports': ports,
+        'monitor': monitor,
         'vlan': vlan
     }
 
 @pytest.fixture(scope='module', autouse=True)
-def setup_monitor_port(duthosts, rand_one_dut_hostname, ports_for_test):
+def setup_monitor_port(duthosts, rand_one_dut_hostname, ports_for_test, cfg_facts):
     '''
     Used to prepare monitor port for test
 
@@ -65,9 +61,10 @@ def setup_monitor_port(duthosts, rand_one_dut_hostname, ports_for_test):
     '''
     duthost = duthosts[rand_one_dut_hostname]
 
-    port = ports_for_test['monitor']['name']
-    tagging_mode = ports_for_test['monitor']['tagging_mode']
+    port = ports_for_test['monitor']
     vlan = ports_for_test['vlan']
+    tagging_mode = cfg_facts['VLAN_MEMBER']['Vlan{}'.format(vlan)][port]['tagging_mode']
+        
 
     # Remove monitor port from vlan members
     duthost.command('config vlan member del {} {}'.format(vlan, port))
@@ -77,8 +74,11 @@ def setup_monitor_port(duthosts, rand_one_dut_hostname, ports_for_test):
     # Add monitor port to vlan members
     duthost.command('config vlan member add {} {} --{}'.format(vlan, port, tagging_mode))
 
+@pytest.mark.parametrize('direction', ['rx', 'tx', 'both'])
+@pytest.mark.parametrize('sources', ['port'])
+#@pytest.mark.parametrize('sources', ['port', 'lag', 'port,port', 'port,lag', 'lag,lag'])
 @pytest.fixture
-def session_info(request, ports_for_test):
+def session_info(request, ports_for_test, sources):
     '''
     Used to generate mirroring session info based on selected ports
 
@@ -89,29 +89,22 @@ def session_info(request, ports_for_test):
     Return:
         dict: mirroring session configuration params and port indices
     '''
-    src1 = ports_for_test['source1']
-    src2 = ports_for_test['source2']
-    dst = ports_for_test['monitor']
-    src = src1['name']
+    src = []
+    ports = ports_for_test['ports'][:]
+    lags = ports_for_test['lags'][:]
 
-    if 'rx' in request.node.name:
-        direction = 'rx'
-    elif 'tx' in request.node.name:
-        direction = 'tx'
-    elif 'both' in request.node.name:
-        direction = 'both'
-    elif 'multiple' in request.node.name:
-        direction = 'rx'
-        src = '{},{}'.format(src1['name'], src2['name'])
+    for source_type in sources.split(','):
+        if 'port' in source_type:
+            src.append(ports.pop(0))
+        elif 'lag' in source_type:
+            src.append(lags.pop(0))
+    dst = ports_for_test['monitor']
 
     return {
-        'session_name':'session_1',
-        'session_destination_port': dst['name'],
-        'destination_index': dst['index'],
-        'session_source_ports': src,
-        'source1_index': src1['index'],
-        'source2_index': src2['index'],
-        'session_direction': direction,
+        'name':'session_1',
+        'monitor': dst,
+        'source_ports': src,
+        'direction': direction,
     }
 
 @pytest.fixture
@@ -128,18 +121,16 @@ def setup_session(duthosts, rand_one_dut_hostname, session_info):
         dict: ptf port indices for session source ports and monitor port
     '''
     duthost = duthosts[rand_one_dut_hostname]
+    src_ports = ",".join(session_info["source_ports"]).rstrip(',')
     # Add mirroring session
     duthost.command('config mirror_session span add {} {} {} {}'.format(
-        session_info["session_name"],
-        session_info["session_destination_port"],
-        session_info["session_source_ports"],
-        session_info["session_direction"]
+        session_info["name"],
+        session_info["destination_port"],
+        src_ports,
+        session_info["direction"]
         )
                    )
-    yield {
-        'source1_index': session_info['source1_index'],
-        'source2_index': session_info['source2_index'],
-        'destination_index': session_info['destination_index']
-    }
+    yield 
+
     # Remove mirroring session
     duthost.command('config mirror_session remove {}'.format(session_info["session_name"]))
--- tests/span/span_helpers.py
+++ tests/span/span_helpers.py
@@ -4,7 +4,9 @@ Helper functions for span tests
 
 import ptf.testutils as testutils
 
-def send_and_verify_mirrored_packet(ptfadapter, src_port, monitor):
+from random import randint
+
+def verify_mirroring_flow(ptfadapter, mirror_session):
     '''
     Send packet from ptf and verify it on monitor port
 
@@ -13,10 +15,52 @@ def send_and_verify_mirrored_packet(ptfadapter, src_port, monitor):
         src_port: ptf port index, from which packet will be sent
         monitor: ptf port index, where packet will be verified on
     '''
-    src_mac = ptfadapter.dataplane.get_mac(0, src_port)
+
+    sources = mirror_session['sources']
+    direction = mirror_session['direction']
+    monitor = mirror_session['monitor']
+    
+    for session_source_port in sources:
+        if 'rx' in direction or 'both' in direction:
+            sorce_port = session_source_port
+            destination_port = get_random_opposite_port(session_source_port)
+            send_and_check_mirrored(ptfadapter,
+                                    sorce_port,
+                                    destination_port,
+                                    monitor)
+        elif 'tx' in direction or 'both' in direction:
+            sorce_port = get_random_opposite_port(session_source_port)
+            destination_port = session_source_port 
+            send_and_check_mirrored(ptfadapter,
+                                    sorce_port,
+                                    destination_port,
+                                    monitor)
+        
+
+def get_random_opposite_port(ports_for_test, port):
+    '''
+    For upstream port, return a random downstream port.
+    Return upstream port if passed downstream port
+
+    Args:
+        ports_for_test: fixture with dict of available upstream/downstream ports
+        port: DUT port name, for which to find an opposite port
+    '''
+    for port_group in ports_for_test.keys():
+        ports = ports_for_test[port_group]
+        if port not in ports and port_group != 'monitor':
+            return ports[randint(0, len(ports)-1)]
+
+def generate_packet(ptfadapter, source_port, dst_port):
+    src_mac = ptfadapter.dataplane.get_mac(0, source_port)
+    dst_mac = ptfadapter.dataplane.get_mac(0, destination_port)
 
     pkt = testutils.simple_icmp_packet(eth_src=src_mac, eth_dst='ff:ff:ff:ff:ff:ff')
+    #pkt = testutils.simple_icmp_packet(eth_src=src_mac, eth_dst=dst_mac)
+    return pkt
 
+def send_and_check_mirrored(ptfadapter, src, monitor):
     ptfadapter.dataplane.flush()
     testutils.send(ptfadapter, src_port, pkt)
-    testutils.verify_packet(ptfadapter, pkt, monitor)
+    testutils.verify_packet(ptfadapter, pkt, mirror_session['monitor'])
+
